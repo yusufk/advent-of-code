7d350454acfc71c63ed6884b5b09100b73fe518a
Yusuf Kaka <yusuf.kaka@xbiglylabs.co.za>
2025-12-10 21:50:32 +0200

    day 1 part 1

---
+++ day1/day1.py
@@ -0,0 +1,20 @@
+codes = open("test.txt").read().splitlines()
+start = 50
+passwd = 0
+for code in codes:
+    if code[0] == "L":
+        direction = -1
+    else:
+        direction = 1
+    clicks = int(code[1:])
+    start += direction * clicks
+    while start > 99:
+        start -= 100
+#        passwd += 1
+    while start < 0:
+        start += 100
+#        passwd += 1
+    if start == 0:
+        passwd += 1
+    print(start)
+print("Passes:", passwd)
